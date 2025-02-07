c20bcca4bf3be4f1c151e8113acf0fffc5281353
Kevan Tamboli <kevantamboli@gmail.com>
2025-02-07 22:48:07 +0530

    Set the view engine and added the ejs files for "view"

--- Real-Sauce/app.js
+++ Real-Sauce/app.js
@@ -1,2 +1,31 @@
 const express = require("express")
-const ejs = require("ejs")
+const http = require("http")
+const {Chess} = require("chess.js")
+const socket = require("socket.io")
+const path = require("path")
+const { title } = require("process")
+// got the modeuls and Class(chess wali)
+
+const app = express() // the xpress app managing the routing
+
+//*
+const server = http.createServer(app) // a http server having express app in it managing the routing (did for socket)
+const io = socket(server)
+
+const chess = new Chess()  
+// having all the chess rules from chess.js
+
+let players = {}
+let currPlayer = "W"
+// set the vars
+
+app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname,"public")))
+
+app.get("/",(req,res)=>{
+    res.render(`index`,{title:"Chess Game"})
+})
+
+app.listen(3000,()=>{
+    console.log('http://localhost:3000')
+})
